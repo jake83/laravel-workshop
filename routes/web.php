71e8cce08b85e0a83de3bce284c03a725d54ff87
jake83 <jwatson1983@gmail.com>
2025-11-18 17:45:51 +0000

    fixed assets and extracted replies

--- routes/web.php
+++ routes/web.php
@@ -23,10 +23,24 @@ str,
                 'avatar' => '/images/michael.png',
                 'displayName' => 'Michael',
                 'handle' => '@mmich_jj'
+            ],
+            'replies' => [
+                [
+                    'content' => '<p>Heh - this looks just like me!</p>',
+                    'postedDateTime' => '1h',
+                    'likeCount' => 52,
+                    'replyCount' => 12,
+                    'repostCount' => 200,
+                    'profile' => [
+                        'avatar' => '/images/simon-chilling.png',
+                        'displayName' => 'Simon',
+                        'handle' => '@simonswiss'
+                    ],
+                ]
             ]
         ]
     ]));
-
+    
     return view('feed', compact('feedItems'));
 });
 
@@ -47,6 +61,20 @@ str,
                 'avatar' => '/images/michael.png',
                 'displayName' => 'Michael',
                 'handle' => '@mmich_jj'
+            ],
+            'replies' => [
+                [
+                    'content' => '<p>Heh - this looks just like me!</p>',
+                    'postedDateTime' => '1h',
+                    'likeCount' => 52,
+                    'replyCount' => 12,
+                    'repostCount' => 200,
+                    'profile' => [
+                        'avatar' => '/images/simon-chilling.png',
+                        'displayName' => 'Simon',
+                        'handle' => '@simonswiss'
+                    ],
+                ]
             ]
         ]
     ]));
